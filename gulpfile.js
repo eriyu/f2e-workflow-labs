617e89097d5e8ebd99f5b96fecc4b6cbea38b7f8
lala.lee <lala.lee.goo@gmail.com>
2015-09-05 15:08:18 +0800

    依序執行 mytask2, mytask1, default 工作

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
 var gulp = require('gulp');
-gulp.task('default',['mytask1','mytask2'], function() {
+
+gulp.task('default',['mytask1'], function() {
 	// place code for your default task here
 	console.log('My Default task');
 });
 
-gulp.task('mytask1',function(cb) {
+gulp.task('mytask1',['mytask2'],function() {
     console.log('Task1 Output');
-    //cb();
 });
 
-gulp.task('mytask2',function(cb) {
+gulp.task('mytask2',function() {
     console.log('Task2 Output');
-    cb();
 });
